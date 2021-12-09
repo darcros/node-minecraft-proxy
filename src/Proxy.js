ddd62d15a550317c87b3af31042160019f5ed694
leopoldhub <leopold12.hub@gmail.com>
2021-12-09 22:08:22 +0100

    refactoring the way servers are stored

--- src/Proxy.js
+++ src/Proxy.js
@@ -7,7 +7,7 @@ class Proxy extends mc.Server {
   /**
    * Create a minecraft proxy
    * @param {Object} serverSettings Settings for the minecraft-protocol server
-   * @param {Object} serverList An object that maps a 'serverName' to the server info
+   * @param {Object} serverList The actual list of available servers
    * @param {Object} proxyOptions Proxy settings
    */
   constructor (serverSettings = {}, serverList = {}, proxyOptions = {}) {
@@ -17,8 +17,7 @@ class Proxy extends mc.Server {
     this.autoConnect = (typeof proxyOptions.autoConnect === 'undefined') ? true : !!proxyOptions.autoConnect
     this.autoFallback = (typeof proxyOptions.autoFallback === 'undefined') ? true : !!proxyOptions.autoFallback
 
-    let self = this
-    self.on('login', (remoteClient) => {
+    this.on('login', (remoteClient) => {
       let id = remoteClient.id
 
       remoteClient.on('end', () => {
@@ -30,11 +29,14 @@ class Proxy extends mc.Server {
       })
 
       this.clients[id].isFirstConnection = true
-      this.clients[id].currentServer = ''
+      this.clients[id].currentServer = undefined
 
       if (this.autoConnect) {
-        let defaultServer = this.getDefaultServerName()
-        this.setRemoteServer(id, defaultServer)
+        const defaultServer = this.getDefaultServer()
+        if(!defaultServer)
+          this.emit('error', new Error('No default server found!'))
+        else
+          this.setRemoteServer(id, defaultServer)
       }
     })
   }
@@ -42,83 +44,89 @@ class Proxy extends mc.Server {
   /**
    * Moves the specified client to the specified server
    * @param {number} remoteClientId The ID of the player to move
-   * @param {string} newServerName The name of the server where the player should be moved
+   * @param {Object} newServer The server where the player should be moved
    */
-  setRemoteServer (remoteClientId, newServerName) {
-    let remoteClient = this.clients[remoteClientId]
-    let oldServerName = remoteClient.currentServer
-    let newServer = this.serverList[newServerName]
+  setRemoteServer (remoteClientId, newServer) {
+    const remoteClient = this.clients[remoteClientId]
+    const oldServer = remoteClient.currentServer
 
-    if (remoteClient.currentServer === newServerName) return
+    if (oldServer === newServer) return
 
-    this.emit('playerMoving', remoteClientId, oldServerName, newServerName)
+    this.emit('playerMoving', remoteClientId, oldServer, newServer)
 
-    this.clients[remoteClientId].currentServer = newServerName
+    this.clients[remoteClientId].currentServer = newServer
 
     let newLocalClient = mc.createClient({
-      host: newServer.host,
-      port: newServer.port,
       username: remoteClient.username,
       keepAlive: false, // keep alive is set to false because the remote server will send the packets and the remote client will respond
-      version: remoteClient.version
+      version: remoteClient.version,
+      ...newServer.settings
     })
 
     newLocalClient.on('error', (err) => {
-      this.emit('playerMoveFailed', err, remoteClientId, oldServerName, newServerName)
+      this.emit('playerMoveFailed', err, remoteClientId, oldServer, newServer)
       this.emit('error', err)
-      this.fallback(remoteClientId)
+      try {
+        this.fallback(remoteClientId)
+      } catch (error) {
+        this.emit('error', error)
+      }
     })
 
     if (!remoteClient.isFirstConnection) {
       replayPackets(remoteClient, newLocalClient, () => {
         remoteClient.localClient.end()
         remoteClient.localClient = newLocalClient
-        newLocalClient = undefined
         addListeners(remoteClient, this)
       })
     } else {
       remoteClient.localClient = newLocalClient
-      newLocalClient = undefined
       addListeners(remoteClient, this)
       remoteClient.isFirstConnection = false
     }
 
-    this.emit('playerMoved', remoteClientId, oldServerName, newServerName)
+    this.emit('playerMoved', remoteClientId, oldServer, newServer)
   }
 
   /**
    * Moves the specified client to the fallback server
    * @param {number} remoteClientId The ID of the player to move
+   * @throws {Error} when no fallback server is found
    */
   fallback (remoteClientId) {
-    let oldServerName = this.clients[remoteClientId].currentServer
-    let fallbackServerName = this.getFallbackServerName()
-    this.emit('playerFallback', remoteClientId, oldServerName, fallbackServerName)
-    if (this.autoFallback) this.setRemoteServer(remoteClientId, fallbackServerName)
+    if (this.autoFallback){
+      const oldServer = this.clients[remoteClientId].currentServer
+      const fallbackServer = this.getFallbackServer()
+      if (!fallbackServer) 
+        throw new Error('No fallback server found!')
+      this.emit('playerFallback', remoteClientId, oldServer, fallbackServer)
+      this.setRemoteServer(remoteClientId, fallbackServer)
+    }
   }
 
   /**
-   * Returns the fallback server's name
-   * @returns {string} The fallback server's name
+   * Returns the fallback server
+   * @returns {Object} The fallback server
    */
-  getFallbackServerName () {
-    for (let serverName in this.serverList) {
-      if (this.serverList[serverName].isFallback) return serverName
-    }
-
-    this.emit('error', new Error('No fallback server found!'))
+  getFallbackServer () {
+    return this.serverList.find((server) => server.settings.isFallback);
   }
 
   /**
-   * Returns the default server's name
-   * @returns {string} The default server's name
+   * Returns the default server
+   * @returns {Object} The default server
    */
-  getDefaultServerName () {
-    for (let serverName in this.serverList) {
-      if (this.serverList[serverName].isDefault) return serverName
-    }
+  getDefaultServer () {
+    return this.serverList.find((server) => server.settings.isDefault);
+  }
 
-    this.emit('error', new Error('No default server found!'))
+  /**
+   * Returns the wanted server by name
+   * @param {number} name The wanted server's name
+   * @returns {Object} The server
+   */
+   getServerByName (name) {
+    return this.serverList.find((server) => server.name == name);
   }
 }
 
